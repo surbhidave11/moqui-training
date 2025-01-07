3fb4a1dabb4cc0929aa2a3b51cd7d619060a6e79
Surbhi Dave <surbhidave07@gmail.com>
2025-01-07 14:22:03 +0530

    updated entities of Task 5

--- script/TrainingService.groovy
+++ script/TrainingService.groovy
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.moqui.context.ExecutionContext
 import org.moqui.entity.EntityValue
 
@@ -40,18 +39,6 @@ trainingRecord = trainingRecord.create()
 
 // Set the output parameter
 context.trainingID = trainingRecord.get("trainingId")
-=======
-import org.moqui.context.ExecutionContext
-import org.moqui.entity.EntityValue
-
-// This script assumes it's running in the context of a Moqui service-call
-ExecutionContext ec = context.ec
-
-// Input parameters
-def trainingName = context.trainingName
-def trainingDate = context.trainingDate
-def trainingPrice = context.trainingPrice
-def trainingDuration = context.trainingDuration
 
 // Validate required fields
 if (!trainingName) {
@@ -63,12 +50,6 @@ if (!trainingDate) {
     ec.message.addError("Training date is required.")
     return
 }
-
-// Explicitly generate a unique ID
-def trainingID = ec.entity.sequencedIdPrimary("MoquiTraining", null, null)
-
-// Create the MoquiTraining entity record
-EntityValue trainingRecord = ec.entity.makeValue("moqui.training.MoquiTraining")
 
 trainingRecord.set("trainingID", trainingID) // Explicitly set trainingId
 trainingRecord.set("trainingName", trainingName)
@@ -82,4 +63,4 @@ trainingRecord = trainingRecord.create()
 
 // Set the output parameter
 context.trainingID = trainingRecord.get("trainingID")
->>>>>>> origin/main
+
